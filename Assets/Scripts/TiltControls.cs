704cf283cc8336b7c4cd5eedf08a894231838fb6
Smashface <51160929+Smashface90@users.noreply.github.com>
2019-08-04 03:28:46 +0800

    1.4

---
+++ Assets/Scripts/TiltControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis ("Horizontal") > -.2)
+        {
+            transform.Rotate(0, 0, -1);
+        }
+
+        if (Input.GetAxis("Horizontal") < .2)
+        {
+            transform.Rotate(0, 0, 1);
+        }
+
+        if (Input.GetAxis("Vertical") > -.2)
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+    }
+}
